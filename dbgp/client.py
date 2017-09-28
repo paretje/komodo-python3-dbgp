5c59636a7d9dff5a4ad8ca62fd35b34b86bae079
Kevin Velghe <kevin@paretje.be>
2017-09-28 09:07:19 +0200

    patch client.py to be more compliant
    https://gist.github.com/joonty/3348076#file-client-8-5-0-patch

--- dbgp/client.py
+++ dbgp/client.py
@@ -3433,10 +3433,9 @@ class backendCmd(backend):
         _template = '<response xmlns="urn:debugger_protocol_v1" command="breakpoint_list" transaction_id="%s">%s</response>'
         self.socket.send_response(_template % (tid, bpinfo))
 
-    _eval_optlist = [['i','transaction_id', int, 1, -1, None],
-                ['l','length', int, 1, 0, None]]
+    _eval_optlist = [['i','transaction_id', int, 1, -1, None]]
     def do_eval(self, cmdargs, *args):
-        (tid, data_length, data,) = self._getopts(cmdargs, self._eval_optlist, "eval")
+        (tid, data_length,) = self._getopts(cmdargs, self._eval_optlist, "eval")
         
         # read data_length from the socket
         if self._data_encoding == 'base64':
@@ -3452,8 +3451,8 @@ class backendCmd(backend):
             raise CommandError('eval', tid, ERROR_EVAL_FAILED,
                            'eval of expression failed: '+str(e))
 
-        prop = Property(None, None, value, self._data_encoding,
-                            self._show_hidden, hiddenContextTypes[context_id])
+        prop = Property(data, data, value, self._data_encoding,
+                            self._show_hidden, hiddenContextTypes[0])
         
         _template = '<response xmlns="urn:debugger_protocol_v1" command="eval" transaction_id="%s">%s</response>'
 
